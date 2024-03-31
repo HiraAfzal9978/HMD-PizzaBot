fc90eef51b7981397ec12171809fef1788ac7310
Adnan Irshad <adnanit035@gmail.com>
2024-03-31 13:35:57 +0200

    Added task-1

--- actions.py
+++ actions.py
@@ -1,15 +1,120 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker, FormValidationAction
+from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet, AllSlotsReset, EventType, Form, SlotSet
-from rasa_sdk.types import DomainDict
 
+
+class ActionGreet(Action):
+    def name(self) -> Text:
+        return "utter_greet"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_greet")
+        return []
+
+
+class ActionEndGreet(Action):
+    def name(self) -> Text:
+        return "utter_end_greet"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_end_greet")
+        return []
+
+
+class ActionOutOfScope(Action):
+    def name(self) -> Text:
+        return "utter_out_of_scope"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_out_of_scope")
+        return []
+
+
+class ActionBotChallenge(Action):
+    def name(self) -> Text:
+        return "utter_bot_challenge"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_bot_challenge")
+        return []
+
+
+class ActionAffirm(Action):
+    def name(self) -> Text:
+        return "utter_affirm"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_affirm")
+        return []
+
+
+class ActionDeny(Action):
+    def name(self) -> Text:
+        return "utter_deny"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_deny")
+        return []
+
+
+class ActionInitiateGeneralConversation(Action):
+    def name(self) -> Text:
+        return "utter_initiate_general_conversation"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_initiate_general_conversation")
+        return []
+
+
+class ActionInformMenu(Action):
+    def name(self) -> Text:
+        return "utter_inform_menu"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_inform_menu")
+        return []
+
+
+class ActionInformVegetarianMenu(Action):
+    def name(self) -> Text:
+        return "utter_inform_vegetarian_menu"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_inform_vegetarian_menu")
+        return []
+
+
+class ActionInformNonVegetarianMenu(Action):
+    def name(self) -> Text:
+        return "utter_inform_non_vegetarian_menu"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_inform_non_vegetarian_menu")
+        return []
+
+
+class ActionRecommendPizza(Action):
+    def name(self) -> Text:
+        return "utter_recommend_pizza"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_recommend_pizza")
+        return []
+
+
+class ActionRecommendVegetarianPizza(Action):
+    def name(self) -> Text:
+        return "utter_recommend_vegetarian_pizza"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_recommend_vegetarian_pizza")
+        return []
+
+
+class ActionRecommendNonVegetarianPizza(Action):
+    def name(self) -> Text:
+        return "utter_recommend_non_vegetarian_pizza"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_recommend_non_vegetarian_pizza")
+        return []

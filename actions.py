b1715254378b822595886c465347b0a4cb79a5e3
Adnan Irshad <adnanit035@gmail.com>
2024-04-28 04:28:08 +0200

    Fix: Fixed issues for custom toppings

--- actions.py
+++ actions.py
@@ -2,6 +2,8 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet, FollowupAction
+from typing import Dict, Text, Any, List, Union, Optional
+
 
 PIZZA_TYPES = ["margherita", "supreme", "pesto and sun-dried tomato", "pepperoni", "seafood", "bbq chicken"]
 PIZZA_SIZES = ["small", "medium", "large"]
@@ -24,172 +26,6 @@ PIZZA_PRICES = {
 }
 
 
-class ActionGreet(Action):
-    def name(self) -> Text:
-        return "utter_greet"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_greet")
-        return []
-
-
-class ActionEndGreet(Action):
-    def name(self) -> Text:
-        return "utter_end_greet"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_end_greet")
-        return []
-
-
-class ActionOutOfScope(Action):
-    def name(self) -> Text:
-        return "utter_out_of_scope"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_out_of_scope")
-        return []
-
-
-class ActionBotChallenge(Action):
-    def name(self) -> Text:
-        return "utter_bot_challenge"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_bot_challenge")
-        return []
-
-
-class ActionAffirm(Action):
-    def name(self) -> Text:
-        return "utter_affirm"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_affirm")
-        return []
-
-
-class ActionDeny(Action):
-    def name(self) -> Text:
-        return "utter_deny"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_deny")
-        return []
-
-
-class ActionInitiateGeneralConversation(Action):
-    def name(self) -> Text:
-        return "utter_initiate_general_conversation"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_initiate_general_conversation")
-        return []
-
-
-class ActionInformMenu(Action):
-    def name(self) -> Text:
-        return "utter_inform_menu"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_inform_menu")
-        return []
-
-
-class ActionInformVegetarianMenu(Action):
-    def name(self) -> Text:
-        return "utter_inform_vegetarian_menu"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_inform_vegetarian_menu")
-        return []
-
-
-class ActionInformNonVegetarianMenu(Action):
-    def name(self) -> Text:
-        return "utter_inform_non_vegetarian_menu"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_inform_non_vegetarian_menu")
-        return []
-
-
-class ActionRecommendPizza(Action):
-    def name(self) -> Text:
-        return "utter_recommend_pizza"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_recommend_pizza")
-        return []
-
-
-class ActionRecommendVegetarianPizza(Action):
-    def name(self) -> Text:
-        return "utter_recommend_vegetarian_pizza"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_recommend_vegetarian_pizza")
-        return []
-
-
-class ActionRecommendNonVegetarianPizza(Action):
-    def name(self) -> Text:
-        return "utter_recommend_non_vegetarian_pizza"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_recommend_non_vegetarian_pizza")
-        return []
-
-
-class ActionAskPizzaType(Action):
-    def name(self) -> Text:
-        return "action_ask_pizza_type"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_ask_pizza_type")
-        return []
-
-
-class ActionAskPizzaTopping(Action):
-    def name(self) -> Text:
-        return "action_ask_pizza_topping"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        pizza_type = tracker.get_slot("pizza_type").lower()
-
-        if pizza_type:
-            dispatcher.utter_message(
-                text="The standard toppings for {} are: {}.".format(pizza_type, PIZZA_TOPPINGS_STD[pizza_type]),
-                response="utter_ask_topping_confirmation"
-            )
-
-            # extract user's intent
-            intent = tracker.latest_message['intent'].get('name')
-            print("Intent: ", intent)
-            if intent == "pizza_standard_topping_confirm":
-                # if the user confirms the standard toppings, then proceed with the standard toppings
-                return [SlotSet("pizza_topping", PIZZA_TOPPINGS_STD[pizza_type])]
-            elif intent == "pizza_custom_topping_confirm":
-                # if the user wants custom toppings, then ask for the custom toppings
-                # todo: implement the custom toppings
-                pass
-            else:
-                dispatcher.utter_message(response="utter_ask_topping_confirmation")
-                return []
-        else:
-            dispatcher.utter_message(text="Sorry, I didn't get that. Please choose a pizza type from the available menu.", response="utter_inform_menu")
-            return []
-
-
-class ActionAskToppingConfirmation(Action):
-    def name(self) -> Text:
-        return "action_ask_topping_confirmation"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_ask_topping_confirmation")
-        return []
-
-
 class ActionAskPizzaSize(Action):
     def name(self) -> Text:
         return "action_ask_pizza_size"
@@ -208,14 +44,62 @@ class ActionAskPizzaQuantity(Action):
         return []
 
 
-# create a FormAction class
-class PizzaOrderForm(FormValidationAction):
+class ActionAskPizzaTopping(Action):
     def name(self) -> Text:
-        return "pizza_order_form"
+        return "action_ask_pizza_topping"
 
-    async def required_slots(self, slots_mapped_in_domain: List[Text], dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Text]:
-        return ["pizza_type", "pizza_topping", "pizza_size", "pizza_quantity"]
-    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        pizza_type = tracker.get_slot("pizza_type").lower()
+
+        if pizza_type:
+            dispatcher.utter_message(
+                text="The standard toppings for {} are: {}.".format(pizza_type, PIZZA_TOPPINGS_STD[pizza_type]),
+                response="utter_ask_topping_confirmation"
+            )
+            # dispatcher.utter_message(response="utter_ask_topping_confirmation")
+            return [
+                SlotSet("pizza_topping", PIZZA_TOPPINGS_STD[pizza_type]),
+                FollowupAction("action_ask_topping_confirmation")
+            ]
+        else:
+            dispatcher.utter_message(text="Sorry, I didn't get that. Please choose a pizza type from the available menu.", response="utter_inform_menu")
+            return []
+
+
+class ActionAskToppingConfirmation(Action):
+    def name(self) -> Text:
+        return "action_ask_topping_confirmation"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(
+            text="🍕 Would you like to go with the standard toppings or customize your own toppings?",
+            response="utter_ask_custom_toppings"
+        )
+
+        return []
+
+
+class ActionAskPizzaCustomToppings(Action):
+    def name(self) -> Text:
+        return "action_ask_pizza_custom_toppings"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(response="utter_ask_custom_toppings")
+        return []
+
+
+class ActionSetStandardToppingsNull(Action):
+    def name(self) -> Text:
+        return "action_set_standard_toppings_null"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [SlotSet("pizza_topping", None)]
+
+
+class ActionValidatePizzaOrderForm(FormValidationAction):
+    def name(self) -> Text:
+        return "action_validate_pizza_order_form"
+
     async def validate_pizza_type(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> Dict[Text, Any]:
         """Validate pizza_type value."""
         if slot_value.lower() in PIZZA_TYPES:
@@ -227,17 +111,14 @@ class PizzaOrderForm(FormValidationAction):
     async def validate_pizza_topping(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> Dict[Text, Any]:
         """Validate pizza_topping value."""
         pizza_type = tracker.get_slot("pizza_type").lower()
+        pizza_topping = slot_value.split(",")
+        pizza_topping = [topping.strip() for topping in pizza_topping]
 
-        print("Events: ", tracker.events)
-
-        # check user latest intent to confirm the standard toppings or custom toppings
-        intent = tracker.latest_message['intent'].get('name')
-        print("Latest Intent: ", intent)
-        if intent == "pizza_standard_topping_confirm":
-            return {"pizza_topping": PIZZA_TOPPINGS_STD[pizza_type]}
-        elif intent == "pizza_custom_topping_confirm":
-            # todo: implement the custom toppings
-            pass
+        if all(topping in PIZZA_TOPPINGS_STD[pizza_type] for topping in pizza_topping):
+            return {"pizza_topping": pizza_topping}
+        else:
+            dispatcher.utter_message(text="Sorry, we don't have that topping: {}. Please choose from the available toppings.".format(slot_value), response="utter_ask_topping_confirmation")
+            return {"pizza_topping": None}
 
     async def validate_pizza_size(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> Dict[Text, Any]:
         """Validate pizza_size value."""
@@ -257,15 +138,67 @@ class PizzaOrderForm(FormValidationAction):
             dispatcher.utter_message(text="Sorry, if you want to order more than 10 pizzas, then please contact us on +XX-XXX-XXX-XXXX")
             return {"pizza_quantity": None}
 
-    async def submit(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        """Submit the form."""
+
+class ActionSubmitPizzaOrderForm(Action):
+    def name(self) -> Text:
+        return "action_submit_pizza_order_form"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         pizza_type = tracker.get_slot("pizza_type").lower()
-        pizza_topping = tracker.get_slot("pizza_topping").lower()
         pizza_size = tracker.get_slot("pizza_size").lower()
         pizza_quantity = tracker.get_slot("pizza_quantity").lower()
+        pizza_topping = tracker.get_slot("pizza_topping")
 
-        # calculate the price
-        pizza_price = PIZZA_PRICES[pizza_type][pizza_size] * PIZZA_QUANTITY.index(pizza_quantity) + 1
-        dispatcher.utter_message(response="utter_submit", pizza_type=pizza_type, pizza_topping=pizza_topping,
-                                 pizza_size=pizza_size, pizza_quantity=pizza_quantity, pizza_price=pizza_price)
+        if pizza_type and pizza_size and pizza_quantity:
+            pizza_price = PIZZA_PRICES[pizza_type][pizza_size]
+            pizza_quantity = PIZZA_QUANTITY.index(pizza_quantity) + 1
+            total_price = pizza_price * pizza_quantity
+
+            dispatcher.utter_message(
+                text="You have ordered {} {} {} pizza(s) with {} topping(s). Your total bill is ${:.2f}.".format(
+                    pizza_quantity, pizza_size, pizza_type, len(pizza_topping), total_price
+                )
+            )
+        else:
+            dispatcher.utter_message(text="Sorry, I didn't get that. Please provide all the required information.")
+            return [FollowupAction("action_restart")]
+
         return []
+
+
+class ActionValidatePizzaCustomToppingForm(FormValidationAction):
+    def name(self) -> Text:
+        return "action_validate_pizza_custom_topping_form"
+
+    async def required_slots(self, slots_mapped_in_domain: List[Text], dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> Optional[List[Text]]:
+        return ["pizza_custom_toppings"]
+
+    async def validate_pizza_custom_toppings(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> Dict[Text, Any]:
+        """Validate pizza_custom_toppings value."""
+        pizza_type = tracker.get_slot("pizza_type").lower()
+        pizza_topping = tracker.get_slot("pizza_topping")
+
+        print("pizza_type: ", pizza_type)
+        print("pizza_topping: ", pizza_topping)
+
+        # set pizza_custom_toppings slot with value from pizza_topping slot
+        if pizza_topping:
+            return {"pizza_custom_toppings": pizza_topping}
+        else:
+            dispatcher.utter_message(text="Sorry, I didn't get that. Please provide the custom toppings for your pizza.")
+            return {"pizza_custom_toppings": None}
+
+
+class ActionSubmitPizzaCustomToppingForm(Action):
+    def name(self) -> Text:
+        return "action_submit_pizza_custom_topping_form"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        pizza_topping = tracker.get_slot("pizza_topping")
+        pizza_topping = pizza_topping.split(",")
+        pizza_topping = [topping.strip() for topping in pizza_topping]
+
+        return [
+            SlotSet("pizza_topping", pizza_topping),  # set the slot value
+            FollowupAction("pizza_order_form")  # go back to the pizza_order_form
+        ]
